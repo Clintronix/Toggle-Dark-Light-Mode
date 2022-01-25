fca58199ecbc961686fa2e69d86f11df83a53e6e
Clintronix <49820287+Clintronix@users.noreply.github.com>
2022-01-25 08:50:11 -0800

    Updated Toggle size and set style to nothing
    Set style to nothing to avoid whiting out images plus this brings all links etc to original state

--- Toggle-DOM-Elements-Dark-Light.js
+++ Toggle-DOM-Elements-Dark-Light.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toggle Dark Light Mode
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Change DOM text to black or white
 // @author       Clinton Cregger
 // @match        http*://*.com/*
@@ -16,6 +16,8 @@
     let allElem = document.querySelectorAll('*');
     let anchorTags = document.querySelectorAll('a');
 
+
+
     //create toggle switch
     let toggleBtn = document.createElement('input');
     toggleBtn.setAttribute('type', 'checkbox');
@@ -23,38 +25,41 @@
     toggleBtn.style.color = 'black';
     toggleBtn.style.position = 'absolute';
     toggleBtn.style.top = '0px';
-    toggleBtn.style.zIndex = "99";
+    toggleBtn.style.height = '30px';
+    toggleBtn.style.width = '100px';
+    toggleBtn.style.zIndex = '99';
 
 
     let handleToggle = (event)=> {
         if (toggleBtn.checked) {
             //turns bg black and text white including anchor tags
             for (let elem of allElem) {
+                //if (elem.nodeName === 'img') return;
                 elem.style.backgroundColor = 'black';
                 elem.style.color = 'white';
             }
             //loop through all anchor tags change to light blue
             for (let anchor of anchorTags) {
-                if (anchor.value !== "") {
+                if (!anchor.value) {
                     anchor.style.color = "#ADD8E6";
                 }
             }
         } else {
-            //turns bg black and text white including anchor tags
+            //set style to nothing
             for (let elem of allElem) {
-                elem.style.backgroundColor = 'white';
-                elem.style.color = "black";
+                elem.style.backgroundColor = '';
+                elem.style.color = '';
             }
             //loop through all anchor tags change to light blue
             for (let anchor of anchorTags) {
-                if (anchor.value !== "") {
-                    anchor.style.color = "blue";
+                if (!anchor.value) {
+                    anchor.style.color = '';
                 }
             }
         }
 
     }
     //listen for click event on button
-    document.addEventListener('click', handleToggle);
+    toggleBtn.addEventListener('click', handleToggle);
 
 })();
